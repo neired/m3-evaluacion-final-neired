a5055c6b0a6628145e3452cb77a0e48f3fa823ea
neired <nmillans91@gmail.com>
2019-09-23 08:33:16 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
