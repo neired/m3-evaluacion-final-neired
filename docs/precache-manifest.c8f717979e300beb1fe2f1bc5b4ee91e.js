850c1fcdfae20f48032e61b6ebb6aa23fc06753f
neired <nmillans91@gmail.com>
2019-09-24 09:38:49 +0200

    Publish

---
+++ docs/precache-manifest.c8f717979e300beb1fe2f1bc5b4ee91e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03cc038cf5338fa935ae5404aebac2f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "76e46e07508642c0063b",
+    "url": "./static/css/main.b62f5fa9.chunk.css"
+  },
+  {
+    "revision": "000c784bda413d18895b",
+    "url": "./static/js/2.852749f4.chunk.js"
+  },
+  {
+    "revision": "76e46e07508642c0063b",
+    "url": "./static/js/main.1ce750c8.chunk.js"
+  },
+  {
+    "revision": "ca3159b570527cacfb9d",
+    "url": "./static/js/runtime-main.e52036ef.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo.75509f2e.png"
+  }
+]);
